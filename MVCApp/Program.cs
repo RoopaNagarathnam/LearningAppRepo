e518c81b1ab3a1888d6b4ed98cb40e41f7131c43
Roopa NAGARATHNAM <roopa.nagarathnam2710az@gmail.com>
2024-08-05 12:44:21 +0530

    added azureappconfiguration

--- MVCApp/Program.cs
+++ MVCApp/Program.cs
@@ -1,7 +1,9 @@
 using MVCApp;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Configuration.AddAzureAppConfiguration("Endpoint=https://myappconfiguration08022024.azconfig.io;Id=aX37;Secret=A05e600tRwpaWKkdFDuY0epbEt9WKJyKvGh2h6x5nM6vY0qwarYDJQQJ99AHACi5YpzNU05oAAACAZAC0fdH");
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<EmployeeRepository>(); // Register the repository
--- MVCApp/obj/Debug/net8.0/MVCApp.AssemblyInfo.cs
+++ MVCApp/obj/Debug/net8.0/MVCApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVCApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a69d8d6673dd76329ee426b80330897778f6319c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f94da4d259c80076936bff886bf2a6aeca184d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVCApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVCApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
