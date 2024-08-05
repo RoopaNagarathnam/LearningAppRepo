1d300cb6c5b1278e00f72774594b22271c9068da
Roopa NAGARATHNAM <roopa.nagarathnam2710az@gmail.com>
2024-08-05 14:38:33 +0530

    removed feature flag

--- MVCApp/Program.cs
+++ MVCApp/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddAzureAppConfiguration(
     options => { 
         options.Connect("Endpoint=https://myappconfiguration08022024.azconfig.io;Id=aX37;Secret=A05e600tRwpaWKkdFDuY0epbEt9WKJyKvGh2h6x5nM6vY0qwarYDJQQJ99AHACi5YpzNU05oAAACAZAC0fdH");
-        options.UseFeatureFlags();
+        //options.UseFeatureFlags();
     });
 
 
